1e1495f0eb286f2330e8d1ff98c9fcd756512987
Randy Pruim <rpruim@calvin.edu>
2007-05-31 11:58:34 +0000

    statistical tests of randomness now have more power

--- test/unit/random_bit_generator_test.rb
+++ test/unit/random_bit_generator_test.rb
@@ -1,31 +1,40 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class RandomBitGeneratorTest < Test::Unit::TestCase
-
+  
   def test_random_bit
-    counts = generate_random_bits(1000)
-    assert_greater_than counts[0], 400
-    assert_greater_than counts[1], 400
-  end
+    counts = generate_random_bits(5000)
+    # these tests should fail with probability less than 4 * 10^-5
+    assert_greater_than counts[0], 2500 - 4 * standard_error_for_count(5000,0.5)
+    
+    assert_greater_than counts[1], 2500 - 4 * standard_error_for_count(5000,0.5)
+    assert_equal 5000, counts[0] + counts[1]
 
+  end
+  
   def test_random_bit_with_extreme_parameters
     counts = generate_random_bits(1000, 0)
     assert_equal 1000, counts[0]
     assert_equal 0, counts[1]    
-
+    
     counts = generate_random_bits(1000, 1)
     assert_equal 0, counts[0]
-    assert_equal 1000, counts[1]    
+    assert_equal 1000, counts[1]
   end
   
   def test_random_bit_with_typical_parameters
-    counts = generate_random_bits(1000, 0.2)
-    assert_greater_than counts[0], 700
-    assert_greater_than counts[1], 100
+    counts = generate_random_bits(5000, 0.2)
+    # these tests should fail with probability less than 4 * 10^-5
+    assert_greater_than counts[0], 5000 * 0.8 - 4 * standard_error_for_count(5000,0.2)
+    assert_greater_than counts[1], 5000 * 0.2 - 4 * standard_error_for_count(5000,0.2)
+    assert_equal 5000, counts[0] + counts[1]
+    
+    counts = generate_random_bits(5000, 0.9)
+    # these tests should fail with probability less than 4 * 10^-5
+    assert_greater_than counts[0],  0.1 * 5000 - 4 * standard_error_for_count(5000,0.9)
+    assert_greater_than counts[1],  0.9 * 5000 - 4 * standard_error_for_count(5000,0.9)
+    assert_equal 5000, counts[0] + counts[1]
 
-    counts = generate_random_bits(1000, 0.9)
-    assert_greater_than counts[0],  50
-    assert_greater_than counts[1], 850
   end
   
   # 
@@ -49,5 +58,9 @@ class RandomBitGeneratorTest < Test::Unit::TestCase
       expected < actual
     end
   end
-
+  
+  def standard_error_for_count(n,p)
+    Math.sqrt(p * (1-p) * n)
+  end
+  
 end

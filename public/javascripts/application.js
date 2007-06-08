fda5238c836a99ee809e79338ff483db85bc498c
Brian Ryckbost <bryckbost@gmail.com>
2007-06-08 16:57:22 +0000

    Added ability to toggle certain sections in view vial

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// The following code is from CruiseControl.rb
+// A shameless copy of their code.
+function toggle_section(section) {
+  if (section.className == "section_open")
+    section.className = "section_closed"
+  else
+    section.className = "section_open"
+}

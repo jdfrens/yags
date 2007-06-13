fe712ea6bb8d2c6df05606af9c9d9fd153b56fb4
Brian Ryckbost <bryckbost@gmail.com>
2007-06-13 19:05:23 +0000

    Stupid subclipse... Changed how some of the links were working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   acts_as_login_controller
   
-  restrict_to :manage_student, :only => [ :list_users, :add_student ]
+  restrict_to :manage_student, :only => [ :list_users, :add_student, :index ]
 
   redirect_after_login do |controller|
     { :controller => 'bench', :action => 'index' }
+  end
+  
+  def index
+    @username = current_user.username
   end
   
   def list_users
